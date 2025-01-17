1b2b3a0aa0db647ac84299676d5809f8ac2743f0
Natanna99 <erzafairy79@gmail.com>
2025-01-17 17:35:00 -0300

    Feature: criacao do componente de menu mobile com styled

---
+++ src/components/IconeMenuApp/index.jsx
@@ -0,0 +1,50 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import SideMenu from "../SideMenu";
+import { useState } from "react";
+
+const ContainerIconMenuApp = styled.div`
+  display: flex;
+
+  .iconMenu {
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const ContainerMenu = styled.div`
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 20;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const IconeMenuApp = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <>
+      <ContainerIconMenuApp>
+        <FontAwesomeIcon
+          icon="fa-solid fa-bars"
+          className="iconMenu"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+      </ContainerIconMenuApp>
+      <ContainerMenu>
+        {menuOpen && <SideMenu click={setMenuOpen} open={menuOpen} />}
+      </ContainerMenu>
+    </>
+  );
+};
+
+export default IconeMenuApp;
